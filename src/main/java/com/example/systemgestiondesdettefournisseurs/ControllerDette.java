b33a357412b1ffb0f142df03d6b5aea7909c41d2
BakayokoJunior <bakbassjunior@gmail.com>
2023-11-01 16:03:47 +0100

    Création la interface Dette

---
+++ src/main/java/com/example/systemgestiondesdettefournisseurs/ControllerDette.java
@@ -0,0 +1,29 @@
+package com.example.systemgestiondesdettefournisseurs;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ControllerDette implements Initializable {
+
+
+
+
+
+    public void EnregistrerDette(ActionEvent actionEvent) {
+    }
+
+    public void EnregistrerDetteReset(ActionEvent actionEvent) {
+    }
+
+
+
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
